4ad2840f87cb204cd92351ccda48acc8154f796a
Kinga <niu.ula.a@poczta.fm>
2020-04-02 16:00:24 +0200

    Data Storage

---
+++ Assets/Part4/scripts/core/DataStorage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStorage : MonoBehaviour
+{
+    public GameInfo gameInfo;
+
+    private void Start()
+    {
+        Load();  
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();  
+    }
+
+    [ContextMenu("Save")]
+    public void Save()
+    {
+        PlayerPrefs.SetString(Keys.SAVE_KEY, JsonUtility.ToJson(gameInfo));
+        Debug.LogFormat("<color=green>{0}</color>", JsonUtility.ToJson(gameInfo)); 
+    }
+
+
+    [ContextMenu("Load")]
+    public void Load()
+    {
+        gameInfo = JsonUtility.FromJson<GameInfo>(PlayerPrefs.GetString(Keys.SAVE_KEY)); 
+    }
+}
+
+public static class Keys
+{
+    public const string SAVE_KEY = "Save"; 
+    public const string LOAD_KEY = "Load"; 
+}
---
+++ Assets/Part4/scripts/core/GameInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameInfo
+{
+    [SerializeField]
+    private string gameName;
+    public string GameName => gameName;
+
+    [SerializeField]
+    private float gameDuration;
+    public float GameDuration => gameDuration;
+
+    [SerializeField]
+    private Color playerColor;
+    public Color PlayerColor => playerColor; 
+
+}
